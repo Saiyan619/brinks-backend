521f89365f5aabda78937636df72c7fcbc4ac6b2
saiyan619 <arokoyueb11@gmail.com>
2026-09-10 16:02:31 +0100

    updating cors

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,19 @@ async fn main() {
     .with_max_level(tracing::Level::DEBUG)
     .init();
 
+    dotenv::dotenv().ok();
+    let prod_frontend_url = std::env::var("PROD_FRONTEND_URL")
+        .expect("PROD_FRONTEND_URL must be set");
+
     let cors = CorsLayer::new()
     .allow_methods([Method::GET, Method::POST])
-    .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+    .allow_origin([
+        "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+        prod_frontend_url.parse::<HeaderValue>().expect("PROD_FRONTEND_URL must be a valid origin"),
+    ])
     .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
     .allow_credentials(true);
 
-    dotenv::dotenv().ok();
     //Creating a simple server in rust using axum
     // let (group_tx, _rx) = broadcast::channel(100);
     let tx = Mutex::new(HashMap::new());
